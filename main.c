d4123ce5a13c7dc7e23d60140b3a6c3d88260313
ykaique <ygorkaique@gmail.com>
2020-12-05 01:22:28 -0300

    fuse parcial

--- main.c
+++ main.c
@@ -1,7 +1,87 @@
+#define FUSE_USE_VERSION 26
+
+#include <fuse.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
+
+//**************************
+//METODOS FEITOS PELO MANO DO GIT
+//**************************
+
+static const char *filepath = "/file";
+static const char *filename = "file";
+static const char *filecontent = "I'm the content of the only file available there\n";
+
+static int getattr_callback(const char *path, struct stat *stbuf) {
+  memset(stbuf, 0, sizeof(struct stat));
+
+  if (strcmp(path, "/") == 0) {
+    stbuf->st_mode = S_IFDIR | 0755;
+    stbuf->st_nlink = 2;
+    return 0;
+  }
+
+  if (strcmp(path, filepath) == 0) {
+    stbuf->st_mode = S_IFREG | 0777;
+    stbuf->st_nlink = 1;
+    stbuf->st_size = strlen(filecontent);
+    return 0;
+  }
+
+  return -ENOENT;
+}
+
+static int readdir_callback(const char *path, void *buf, fuse_fill_dir_t filler,
+    off_t offset, struct fuse_file_info *fi) {
+  (void) offset;
+  (void) fi;
+
+  filler(buf, ".", NULL, 0);
+  filler(buf, "..", NULL, 0);
+
+  filler(buf, filename, NULL, 0);
+
+  return 0;
+}
+
+static int open_callback(const char *path, struct fuse_file_info *fi) {
+  return 0;
+}
+
+static int read_callback(const char *path, char *buf, size_t size, off_t offset,
+    struct fuse_file_info *fi) {
+
+  if (strcmp(path, filepath) == 0) {
+    size_t len = strlen(filecontent);
+    if (offset >= len) {
+      return 0;
+    }
+
+    if (offset + size > len) {
+      memcpy(buf, filecontent + offset, len - offset);
+      return len - offset;
+    }
+
+    memcpy(buf, filecontent + offset, size);
+    return size;
+  }
+
+  return -ENOENT;
+}
+
+static struct fuse_operations fuse_example_operations = {
+  .getattr = getattr_callback,
+  .open = open_callback,
+  .read = read_callback,
+  .readdir = readdir_callback,
+};
+
+//**************************
+//METODOS FEITOS PELO HIGOR
+//**************************
 int get_imagem_para_deslocamento(FILE* bmp_deslocamento) {
 	fseek(bmp_deslocamento,10,0);
 	int deslocamento;
@@ -20,6 +100,9 @@ int get_bit(char o_byte,int outro_bit) {
 }
 
 int main(int argc,char** argv) {
+
+
+
 	unsigned char tabela_mascarada[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80 };
 
 	FILE *id_arquivo;
@@ -145,4 +228,6 @@ int main(int argc,char** argv) {
 	/* Limpe antes de sair */
 	fclose(id_arquivo);
 	fclose(id_msg_escondida);
+	return fuse_main(argc, argv, &fuse_example_operations, NULL);
+
 }
